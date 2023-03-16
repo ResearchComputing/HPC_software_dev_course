e3614f30f1ae7694b32118251ee2eae4d7855f7f
monaghaa <33913248+monaghaa@users.noreply.github.com>
2023-03-16 11:38:44 -0600

    Update internal_timing.py

--- hpc_profiling/simple_examples/internal_timing.py
+++ hpc_profiling/simple_examples/internal_timing.py
@@ -15,8 +15,10 @@ def maxabs(a,b):
 
 def main():
     import time
-    ntests = 100
+    ntests = 1000
     n = 16384
+    
+    #initialize
     t1 = time.time()
     a = numpy.zeros(n,dtype='float64')
     b = numpy.zeros(n,dtype='float64')
@@ -25,11 +27,17 @@ def main():
         a[i] = i*i
         b[i] = a[i]*i*(-1)**imod
     t2 = time.time()
-    dt = t2-t2
+    dt = t2-t1
     print('Init time (s): ', dt)
+    
+    #math tests
+    t1 = time.time()
     for i in range(ntests):
         dotprod(a,b)
         absdiff(a,b)
         maxabs(a,b)
+    t2 = time.time()
+    dt = t2-t1
+    print('math test time (s): ', dt)
 
 main()
